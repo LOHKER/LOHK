86860089d41516a8aa848a2d09b73b5f4d207fb1
Corben Broennimann <54660123+corbenbroennimann@users.noreply.github.com>
2020-06-10 20:31:24 -1000

    message

--- app/imports/ui/pages/Dashboard.jsx
+++ app/imports/ui/pages/Dashboard.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Button, Icon, Popup, Card, Container } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+
 
 /** A simple static component to render some text for the landing page. */
 class Dashboard extends React.Component {
   render() {
+    const divStyle = { paddingTop: '20px', paddingBottom: '210px' };
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>DASHBOARD GOES HERE</h1>
-            <h1>THIS IS BASCIALLY THE CENTRAL HOME PAGE</h1>
-          </Grid.Column>
-
-        </Grid>
+        <div style={{ backgroundColor: '#2A427A' }}>
+          <Popup on='click' trigger={<Button inverted animated='fade' size={'medium'}
+                                  style={{ backgroundColor: '#D7B377', marginTop: '25px' }}>
+            <Button.Content style={{ color: 'white' }} visible>
+              <Icon name={'plus circle'}></Icon> ADD INFO
+            </Button.Content>
+            <Button.Content style={{ color: '#385F71' }} inverted hidden>
+              <Icon name={'plus circle'}></Icon> ADD INFO
+            </Button.Content>
+          </Button>}>
+            <Button inverted animated='fade' size={'medium'} as={NavLink} exact to={'/add-account'}
+                    style={{ backgroundColor: '#D7B377' }}>
+              <Button.Content style={{ color: 'white' }} visible>
+                <Icon name={'plus circle'}></Icon> ADD ACCOUNT
+              </Button.Content>
+              <Button.Content style={{ color: '#385F71' }} inverted hidden>
+                <Icon name={'plus circle'}></Icon> ADD ACCOUNT
+              </Button.Content>
+            </Button>
+            <Button inverted animated='fade' size={'medium'} as={NavLink} exact to={'/add-card'}
+                    style={{ backgroundColor: '#D7B377' }}>
+              <Button.Content style={{ color: 'white' }} visible>
+                <Icon name={'plus circle'}></Icon> ADD CREDIT CARD
+              </Button.Content>
+              <Button.Content style={{ color: '#385F71' }} inverted hidden>
+                <Icon name={'plus circle'}></Icon> ADD CREDIT CARD
+              </Button.Content>
+            </Button>
+          </Popup>
+        <Container style={ divStyle }>
+          Content
+        </Container>
+        </div>
     );
   }
 }
